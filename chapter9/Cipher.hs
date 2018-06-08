e3627548c26e07aaaf2a37ad2d41ea18d77b026f
Filippo Costalli <30616504+filippocostalli@users.noreply.github.com>
2018-06-08 16:21:53 +0200

    Add files via upload

--- chapter9/Cipher.hs
+++ chapter9/Cipher.hs
@@ -8,10 +8,8 @@ caesarCipher  s (x: xs) = (chr . (+) 96 $ mod (ord x - 96 + s) 26) : caesarCiphe
 
 
 caesarCipher' :: Int -> [Char] -> [Char]
-caesarCipher' _ [] = []
 caesarCipher'  s xs =  map (\x -> chr ( ( mod (ord x - 96 + s) 26)  + 96 ) )  xs
 
 
 caesarCipher'' :: Int -> [Char] -> [Char]
-caesarCipher'' _ [] = []
 caesarCipher''  s xs =  map (\x -> chr . (+) 96 $ mod (ord x - 96 + s) 26)  xs
