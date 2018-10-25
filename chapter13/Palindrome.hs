ce814366fb440696ebe57b2521f7478cf432d9f7
Filippo Costalli <30616504+filippocostalli@users.noreply.github.com>
2018-10-25 15:39:57 +0200

    Add files via upload

---
+++ chapter13/Palindrome.hs
@@ -0,0 +1,24 @@
+import Control.Monad
+import System.Exit (exitSuccess)
+import Data.Char
+
+
+cleanInput :: [Char] -> [Char]
+cleanInput i = map toLower $ filter (\x -> isLetter x || isNumber x) i
+
+
+exitPalindrome :: [Char] -> IO ()
+exitPalindrome s =
+  if (s == reverse s) then
+    do 
+        putStrLn "It's a palindrome!"
+        exitSuccess
+  else
+    do
+        putStrLn "Nope!" 
+        return ()
+
+palindrome :: IO ()
+palindrome = forever $ do
+    line1 <- getLine
+    exitPalindrome $ cleanInput line1
